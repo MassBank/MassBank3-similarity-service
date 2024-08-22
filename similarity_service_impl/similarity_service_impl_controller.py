63709da6b49b2b3c96f87e04992074764a81fd28
René Meier <meier.rene@googlemail.com>
2024-08-22 15:03:25 +0200

    Move to msp data source

--- similarity_service_impl/similarity_service_impl_controller.py
+++ similarity_service_impl/similarity_service_impl_controller.py
@@ -4,86 +4,37 @@ from datetime import datetime
 
 import connexion
 import numpy
-import psycopg
-from psycopg_pool import ConnectionPool
 from flask import jsonify
 from matchms import calculate_scores, set_matchms_logger_level, Spectrum
 from matchms.filtering import normalize_intensities
 from matchms.similarity import CosineGreedy
 from matchms.importing import load_from_msp
 
+import os.path
+
 from similarity_service.models import SimilarityScore
 from similarity_service.models.similarity_calculation import SimilarityCalculation  # noqa: E501
 from similarity_service.models.similarity_score_list import SimilarityScoreList  # noqa: E501
 
-# Environment variables for database connection
-DB_PORT = os.environ.get('DB_PORT', 5432)
-DB_USER = os.environ.get('DB_USER', "massbank3")
-DB_PASSWORD = os.environ.get('DB_PASSWORD', "massbank3password")
-DB_HOST = os.environ.get('DB_HOST', "localhost")
-DB_NAME = os.environ.get('DB_NAME', "massbank3")
-MASSBANK_DATA = os.environ.get('DB_NAME', "/home/rmeier/GIT/MassBank-data")
+# Environment variables
+MSP = os.environ.get('MSP', "./MassBank_NIST.msp")
 
 # Global variables for in-memory data
 timestamp = datetime.fromisoformat('2010-01-01')
 spectra = []
 
-# Initialize the connection pool globally
-pool = ConnectionPool(conninfo=f"postgresql://{DB_NAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}", open=False)
-
 # Global setting
 logging.basicConfig(level="INFO")
+set_matchms_logger_level("ERROR")
 
-def load_spectra2():
-    global spectra
-    file_msp = "MassBank_NIST.msp"
-    spectra = list(load_from_msp(file_msp))
-
-def load_spectra():  # noqa: E501
-    """load all spectra from the database if the metadata indicates newer data and stores them"""
-    global timestamp, spectra
-    pool.open()
-    with pool.connection() as conn:
-        with conn.cursor() as cur:
-            cur.execute("SELECT * FROM metadata;")
-            dbtimestamp = cur.fetchone()[2]
-        logging.info("Database timestamp: %s", dbtimestamp)
-        logging.info("In-memory timestamp: %s", timestamp)
-        timestamp_diff = dbtimestamp - timestamp
-        if timestamp_diff.total_seconds() > 0:
-            logging.info("Database timestamp is %s newer. Reloading...", timestamp_diff)
-
-            # Prevent matchms from complaining about spectra not having a precursor_mz
-            set_matchms_logger_level("ERROR")
-
-            with conn.cursor() as cur:
-                cur.execute(
-                    "SELECT massbank.accession, peak.mz, peak.intensity "
-                    "FROM massbank "
-                    "JOIN spectrum ON massbank.id = spectrum.massbank_id "
-                    "JOIN peak ON spectrum.id = peak.spectrum_id "
-                    "ORDER BY massbank.id, peak.mz;"
-                )
-
-                entries = {}
-                spectra = []
-
-                for res in cur:
-                    accession, mz, intensity = res
-                    if accession not in entries:
-                        entries[accession] = {"mz": [], "intensity": []}
-                    entries[accession]["mz"].append(mz)
-                    entries[accession]["intensity"].append(intensity)
-
-                for accession, data in entries.items():
-                    spectra.append(
-                        normalize_intensities(Spectrum(mz=numpy.array(data["mz"]).astype(float),
-                                                       intensities=numpy.array(data["intensity"]).astype(float),
-                                                       metadata={'spectrum_id': accession})))
-
-                logging.info("Loaded %s spectra from the database.", len(spectra))
-                timestamp = dbtimestamp
-
+def load_spectra():
+    """load all spectra from the given msp file"""
+    global timestamp, MSP, spectra
+    file_timestamp = datetime.fromtimestamp(os.path.getmtime(MSP))
+    timestamp_diff = file_timestamp - timestamp
+    if timestamp_diff.total_seconds() > 0:
+        spectra = list(load_from_msp(MSP))
+        timestamp = file_timestamp
 
 def similarity_post(similarity_calculation):  # noqa: E501
     """Create a new similarity calculation.
@@ -98,14 +49,14 @@ def similarity_post(similarity_calculation):  # noqa: E501
     if connexion.request.is_json:
         request = SimilarityCalculation.from_dict(similarity_calculation)
 
-        try:
-            load_spectra()
-        except psycopg.DatabaseError as e:
-            return connexion.problem(
-                title="Database Error",
-                detail=str(e),
-                status=500,
-            )
+        # try:
+        load_spectra()
+        # except psycopg.DatabaseError as e:
+        #     return connexion.problem(
+        #         title="Database Error",
+        #         detail=str(e),
+        #         status=500,
+        #     )
 
         mz, intensities = zip(*[(peak.mz, peak.intensity) for peak in request.peak_list])
 
@@ -138,7 +89,7 @@ def version_get():  # noqa: E501
 
     :rtype: Union[str, Tuple[str, int], Tuple[str, int, Dict[str, str]]
     """
-    return 'cosine similarity 1.0.0'
+    return 'similarity similarity 0.1.0'
 
 
 def handle_psycopg_database_error(error):
