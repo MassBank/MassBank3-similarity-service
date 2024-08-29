6f9d5de24ce343420657fbf301d0ce296721e8dc
René Meier <meier.rene@googlemail.com>
2024-08-29 13:57:15 +0200

    Make load_spectra() thread safe

--- similarity_service_impl/similarity_service_impl_controller.py
+++ similarity_service_impl/similarity_service_impl_controller.py
@@ -1,16 +1,14 @@
 import logging
 import os
+import threading
 from datetime import datetime
 
 import connexion
 import numpy
-from flask import jsonify
 from matchms import calculate_scores, set_matchms_logger_level, Spectrum
 from matchms.filtering import normalize_intensities
-from matchms.similarity import CosineGreedy
 from matchms.importing import load_from_msp
-
-import os.path
+from matchms.similarity import CosineGreedy
 
 from similarity_service.models import SimilarityScore
 from similarity_service.models.similarity_calculation import SimilarityCalculation  # noqa: E501
@@ -23,6 +21,9 @@ MSP = os.environ.get('MSP', "./MassBank_NIST.msp")
 timestamp = datetime.fromisoformat('2010-01-01')
 spectra = []
 
+# Lock for thread safety
+lock = threading.Lock()
+
 # Global setting
 logging.basicConfig(level="INFO")
 set_matchms_logger_level("ERROR")
@@ -30,11 +31,17 @@ set_matchms_logger_level("ERROR")
 def load_spectra():
     """load all spectra from the given msp file"""
     global timestamp, MSP, spectra
-    file_timestamp = datetime.fromtimestamp(os.path.getmtime(MSP))
-    timestamp_diff = file_timestamp - timestamp
-    if timestamp_diff.total_seconds() > 0:
-        spectra = list(load_from_msp(MSP))
-        timestamp = file_timestamp
+    with lock:
+        file_timestamp = datetime.fromtimestamp(os.path.getmtime(MSP))
+        logging.info("In-memory timestamp: %s", timestamp)
+        logging.info("Data file timestamp: %s", file_timestamp)
+        timestamp_diff = file_timestamp - timestamp
+        if timestamp_diff.total_seconds() > 0:
+            logging.info("Data file timestamp is %s newer. Reloading...", timestamp_diff)
+            spectra = list(load_from_msp(MSP))
+            timestamp = file_timestamp
+            logging.info("Loaded %s spectra from the data file.", len(spectra))
+
 
 def similarity_post(similarity_calculation):  # noqa: E501
     """Create a new similarity calculation.
@@ -85,7 +92,4 @@ def version_get():  # noqa: E501
     return 'similarity service 0.1'
 
 
-def handle_psycopg_database_error(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+
